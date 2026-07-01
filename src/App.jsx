603e1d5377b743950eb936b9e6a9c287c7dd2d43
maisacaldas <maisacaldas11@email.com>
2026-07-01 10:47:26 -0300

    novo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+export default function App() {
+
+  return (
+
+    <section className="banner">
+      
+      <p className="titulo">Estude CSS com o nosso curso</p>
+      <p>Estilize páginas incríveis e impressione com seus layouts!</p>
+     
+      <button className="btn-primary">Começar agora</button>
+   
+    </section>
+
+  )
+}
